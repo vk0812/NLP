eaf091397bcfc07b62586d3e5a0c518bd1115e0e
Vidit <viditk0812@gmail.com>
2022-03-19 00:50:06 +0530

    Helper Functions

---
+++ functions.py
@@ -0,0 +1,44 @@
+import nltk
+from nltk.corpus import twitter_samples
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer        
+from nltk.tokenize import TweetTokenizer
+import string
+import re
+import numpy as np
+
+nltk.download('stopwords')
+
+def process_tweet(tweet):
+    stemmer = PorterStemmer()
+    stopwords_english = stopwords.words('english')
+    
+    tweet = re.sub(r'\$\w*', '', tweet)
+    tweet = re.sub(r'^RT[\s]+', '', tweet)
+    tweet = re.sub(r'https?:\/\/.*[\r\n]*', '', tweet)
+    tweet = re.sub(r'#', '', tweet)
+    
+    tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
+    tweet_tokens = tokenizer.tokenize(tweet)
+
+    tweets_clean = []
+    for word in tweet_tokens:
+        if (word not in stopwords_english and  # remove stopwords
+                word not in string.punctuation):  # remove punctuation
+            stem_word = stemmer.stem(word)  # stemming word
+            tweets_clean.append(stem_word)
+
+    return tweets_clean
+
+def build_freqs(tweets, ys):
+    yslist = np.squeeze(ys).tolist()
+
+    freqs = {}
+    for y, tweet in zip(yslist, tweets):
+        for word in process_tweet(tweet):
+            pair = (word, y)
+            if pair in freqs:
+                freqs[pair] += 1
+            else:
+                freqs[pair] = 1    
+    return freqs
